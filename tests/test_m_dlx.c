0b06b8a8331b7942d0925c8a5db170b525dbd1a2
jack <Jack Grahl>
2020-11-20 23:15:47 +0000

    More tests on the zeroed out array.

--- tests/test_m_dlx.c
+++ tests/test_m_dlx.c
@@ -65,13 +65,33 @@ test_correct_zeroing_out(const MunitParameter params[], void* data) {
   int row_count = 4;
   int col_count = 3;
   int array[] = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1};
-  int solution[] = {0, 0, 0, -1};
+  int solution[] = {-1, -1, -1, -1};
 
   int length = dlx_get_exact_cover(row_count, col_count, array, solution);
   munit_assert(length == 3);
   munit_assert_int(solution[0], ==, 2);
   munit_assert_int(solution[1], ==, 1);
   munit_assert_int(solution[2], ==, 3);
+  munit_assert_int(solution[3], ==, 0);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+test_correct_zeroing_out_2(const MunitParameter params[], void* data) {
+  (void) params;
+  (void) data;
+
+  int row_count = 4;
+  int col_count = 3;
+  int array[] = {0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1};
+  int solution[] = {-1, -1, -1, -1};
+
+  int length = dlx_get_exact_cover(row_count, col_count, array, solution);
+  munit_assert(length == 2);
+  munit_assert_int(solution[0], ==, 1);
+  munit_assert_int(solution[1], ==, 3);
+  munit_assert_int(solution[2], ==, 0);
   munit_assert_int(solution[3], ==, 0);
 
   return MUNIT_OK;
@@ -84,6 +104,7 @@ static MunitTest test_suite_tests[] = {
   { (char*) "two_row_problem", test_get_two_row_result, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "bigger_simple_problem", test_get_multiple_row_result, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "zero_out_array_correctly", test_correct_zeroing_out, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "zero_out_array_start_and_end", test_correct_zeroing_out_2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
 
