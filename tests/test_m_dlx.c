036a27abc267ec88b15d4f3bf573a88810f8153a
jack <Jack Grahl>
2020-11-20 10:56:51 +0000

    Add tests for the search.

---
+++ tests/test_m_dlx.c
@@ -0,0 +1,57 @@
+#include "munit.h"
+#include "../src/dlx.h"
+
+
+static MunitResult
+test_get_simple_result(const MunitParameter params[], void* data) {
+  (void) params;
+  (void) data;
+
+  int row_count = 1;
+  int col_count = 3;
+  int array[] = {1, 1, 1};
+  int solution[] = {0};
+
+  int length = dlx_get_exact_cover(row_count, col_count, array, solution);
+  munit_assert(length == 1);
+
+  return MUNIT_OK;
+}
+
+/* Creating a test suite is pretty simple.  First, you'll need an
+ * array of tests: */
+static MunitTest test_suite_tests[] = {
+  {
+    (char*) "Very simple exact cover problem",
+    test_get_simple_result,
+    NULL,
+    NULL,
+    MUNIT_TEST_OPTION_NONE,
+    NULL
+  },
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+
+/* Now we'll actually declare the test suite.  You could do this in
+ * the main function, or on the heap, or whatever you want. */
+static const MunitSuite test_suite = {
+  (char*) "my tests",
+  test_suite_tests,
+  NULL,
+  1,
+  MUNIT_SUITE_OPTION_NONE
+};
+
+/* This is only necessary for EXIT_SUCCESS and EXIT_FAILURE, which you
+ * *should* be using but probably aren't (no, zero and non-zero don't
+ * always mean success and failure).  I guess my point is that nothing
+ * about µnit requires it. */
+#include <stdlib.h>
+
+int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+  /* Finally, we'll actually run our test suite!  That second argument
+   * is the user_data parameter which will be passed either to the
+   * test or (if provided) the fixture setup function. */
+  return munit_suite_main(&test_suite, (void*) "µnit", argc, argv);
+}
