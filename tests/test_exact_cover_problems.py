1d9bc947d655d3bba7c31d84516249662bfae68d
jack <Jack Grahl>
2020-11-21 11:41:23 +0000

    Clean up formatting.

--- tests/test_exact_cover_problems.py
+++ tests/test_exact_cover_problems.py
@@ -6,7 +6,13 @@ from hypothesis.strategies import integers, lists, booleans
 from exact_cover_np import get_exact_cover
 
 
-@given(integers(min_value=0, max_value=10).flatmap(lambda n: lists(lists(booleans(), min_size=n, max_size=n), min_size=1, max_size=30)))
+@given(
+    integers(min_value=0, max_value=10).flatmap(
+        lambda n: lists(
+            lists(booleans(), min_size=n, max_size=n), min_size=1, max_size=30
+        )
+    )
+)
 def test_exact_cover(array_data):
     rowcount = len(array_data)
     data = np.array(array_data, dtype=np.int32)
