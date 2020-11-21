34e5975aaa4e5b51a49e8ae15314c85ac21d84b3
jack <Jack Grahl>
2020-11-21 11:54:09 +0000

    Add strategy for problem with solution.

--- tests/test_exact_cover_problems.py
+++ tests/test_exact_cover_problems.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from hypothesis import given, strategies
+from hypothesis import given
 from hypothesis.strategies import integers, lists, booleans
+from hypothesis.strategies import composite, permutations
 
 from exact_cover_np import get_exact_cover
 
@@ -17,5 +18,27 @@ def test_exact_cover(array_data):
     rowcount = len(array_data)
     data = np.array(array_data, dtype=np.int32)
     actual = get_exact_cover(data)
+    assert actual.size <= rowcount
+    assert all(actual < rowcount)
+
+
+@composite
+def array_with_exact_cover(draw):
+    width = draw(integers(min_value=1, max_value=15))
+    dummy_data = draw(lists(lists(booleans(), min_size=width, max_size=width), min_size=1, max_size=30))
+    cover_size = draw(integers(min_value=1, max_value=10))
+    cover = draw(lists(integers(min_value=0, max_value=cover_size - 1), min_size=width, max_size=width))
+    cover_data = [[a == i for a in cover] for i in range(0, cover_size)]
+    data = cover_data + dummy_data
+    shuffled_data = draw(permutations(data))
+    return np.array(shuffled_data, dtype=np.int32)
+
+
+@given(array_with_exact_cover())
+def test_exact_cover_with_solution(array_data):
+    rowcount = len(array_data)
+    data = np.array(array_data, dtype=np.int32)
+    actual = get_exact_cover(data)
+    assert actual.size > 0
     assert actual.size <= rowcount
     assert all(actual < rowcount)
