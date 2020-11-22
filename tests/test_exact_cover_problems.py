cbced64f402cb8f0f01f9b773b6529139bc5a4fc
jack <Jack Grahl>
2020-11-22 01:03:05 +0000

    Add a test for problems without solution.

--- tests/test_exact_cover_problems.py
+++ tests/test_exact_cover_problems.py
@@ -43,3 +43,20 @@ def test_exact_cover_with_solution(array_data):
     assert all(actual < rowcount)
     check_cover = array_data[actual, :].sum(axis=0)
     assert all(check_cover == 1)
+
+
+@composite
+def exact_cover_problem_with_empty_col(draw):
+    width = draw(integers(min_value=1, max_value=15))
+    data = draw(lists(lists(booleans(), min_size=width, max_size=width), min_size=1, max_size=30))
+    array = np.array(data, dtype=np.int32)
+    col = draw(integers(min_value=0, max_value=width-1))
+    array[:, col] = 0
+    return array
+
+
+@given(exact_cover_problem_with_empty_col())
+def test_exact_cover_without_solution(array_data):
+    rowcount = len(array_data)
+    actual = get_exact_cover(array_data)
+    assert actual.size == 0
