3c2a8694cd4563efdb160fd8544a588d4f41f43b
jack <Jack Grahl>
2020-11-23 14:11:52 +0000

    Add the data from the polyomino problem.

--- tests/test_exact_cover.py
+++ tests/test_exact_cover.py
@@ -3,6 +3,7 @@ import numpy as np
 from pytest import mark
 
 from exact_cover_np import get_exact_cover
+from .polyomino_data import polyomino_problem
 
 
 def test_exact_cover():
@@ -37,3 +38,9 @@ def test_single_row_exact_cover():
     expected = np.array([0])
     actual = get_exact_cover(data)
     np.testing.assert_array_equal(actual, expected)
+
+
+def test_complex_exact_cover_problem():
+    data = np.array(polyomino_problem, dtype=np.int32)
+    actual = get_exact_cover(data)
+    assert actual.shape == (13, )
