573fa55dee6a46e3a7bbf4bfa6ef37c67dfc9340
Jack Grahl <jack.grahl@gmail.com>
2021-01-09 14:06:21 +0000

    Add build script.

---
+++ build.py
@@ -0,0 +1,20 @@
+from setuptools import Extension
+
+import numpy
+
+# define the extension module
+deps_dir = "../src/"
+src_dir = "./"
+exact_cover = Extension('exact_cover',
+                           sources=[src_dir + 'exact_cover_np.c',
+                                    deps_dir + 'dlx.c',
+                                    deps_dir + 'sparse_matrix.c',
+                                    deps_dir + 'quad_linked_list.c'],
+                           include_dirs=[deps_dir, numpy.get_include()])
+
+def build(setup_kwargs):
+    setup_kwargs.update(
+        {
+            "ext_modules": [exact_cover],
+        }
+    )
