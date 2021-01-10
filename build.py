1b22891c76e4be10b0df9fde85e5d6dd928c6bbd
Jack Grahl <jack.grahl@gmail.com>
2021-01-10 00:04:34 +0000

    Flake8.

--- build.py
+++ build.py
@@ -7,12 +7,17 @@ import pytest
 # define the extension module
 deps_dir = "src/"
 src_dir = "exact_cover/"
-exact_cover = Extension('exact_cover',
-                           sources=[src_dir + 'exact_cover_np.c',
-                                    deps_dir + 'dlx.c',
-                                    deps_dir + 'sparse_matrix.c',
-                                    deps_dir + 'quad_linked_list.c'],
-                           include_dirs=[deps_dir, numpy.get_include()])
+exact_cover = Extension(
+    'exact_cover',
+    sources=[
+        src_dir + 'exact_cover_np.c',
+        deps_dir + 'dlx.c',
+        deps_dir + 'sparse_matrix.c',
+        deps_dir + 'quad_linked_list.c'
+    ],
+    include_dirs=[deps_dir, numpy.get_include()]
+)
+
 
 def build(setup_kwargs):
     setup_kwargs.update(
@@ -24,4 +29,3 @@ def build(setup_kwargs):
 
 def test():
     pytest.main()
-
--- tests/test_exact_cover.py
+++ tests/test_exact_cover.py
@@ -1,20 +1,24 @@
 import numpy as np
 
-from pytest import mark
-
 from exact_cover import get_exact_cover
 from .polyomino_data import polyomino_problem
 
 
 def test_exact_cover():
-    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]], dtype=np.int32)
+    data = np.array(
+        [[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]],
+        dtype=np.int32
+    )
     expected = np.array([0, 1, 3])
     actual = get_exact_cover(data)
     np.testing.assert_array_equal(actual, expected)
 
 
 def test_exact_cover_no_solution():
-    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 0], [0, 0, 0]], dtype=np.int32)
+    data = np.array(
+        [[1, 0, 0], [0, 1, 0], [0, 1, 0], [0, 0, 0]],
+        dtype=np.int32
+    )
     expected = np.array([])
     actual = get_exact_cover(data)
     np.testing.assert_array_equal(actual, expected)
