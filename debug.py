ef6b9f4b867ec9a03017484bbe55031a79ccf039
Jack Grahl <jack.grahl@gmail.com>
2021-03-16 22:33:47 +0000

    Use a decent polyomino tiling problem as a test.

--- debug.py
+++ debug.py
@@ -7,7 +7,7 @@ from exact_cover import get_exact_cover
 
 def run_debug():
     logging.basicConfig(level=logging.DEBUG)
-    data = np.genfromtxt("tests/files/part_reduced.csv", dtype=np.int32)
+    data = np.genfromtxt("tests/files/pentominos_chessboard.csv", dtype=np.int32)
     logging.info(data.shape)
     solution = get_exact_cover(data, True)
     logging.info(solution)
