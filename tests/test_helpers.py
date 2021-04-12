6d63009fcab6b7cf9fce74925f4b6eb06115cd62
Jack Grahl <jack.grahl@gmail.com>
2021-04-12 23:00:00 +0100

    Correctly check for equality.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,4 +6,5 @@ from exact_cover.helpers import reduce
 def test_reduce():
     d = [[0, 1, 0], [1, 1, 1], [1, 1, 1]]
     a = np.array(d)
-    assert reduce(a) == a     
+    a = reduce(a)
+    np.testing.assert_array_equal(result, a)
