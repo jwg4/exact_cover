f42adaa3888279a18c97dc37f4a07eb1563048eb
Jack Grahl <jack.grahl@gmail.com>
2021-04-12 23:04:29 +0100

    typo a -> result.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,5 +6,5 @@ from exact_cover.helpers import reduce
 def test_reduce():
     d = [[0, 1, 0], [1, 1, 1], [1, 1, 1]]
     a = np.array(d)
-    a = reduce(a)
+    result = reduce(a)
     np.testing.assert_array_equal(result, a)
