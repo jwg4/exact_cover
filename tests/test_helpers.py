c7f5b3d13114db1d36bfbecf0ff0cd6150966d82
Jack Grahl <jack.grahl@gmail.com>
2021-04-13 23:59:50 +0100

    Check that the result the right size.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -14,6 +14,7 @@ def test_split_problem():
     d = [[0, 1, 0], [1, 1, 1], [1, 1, 1]]
     a = np.array(d)
     result = split_problem(a, 2)
-    assert len(result) >= 2 
+    assert len(result) >= 2
     for sub in result:
-        assert isinstance(sub, np.array)
+        assert isinstance(sub, np.ndarray)
+        assert sub.shape == a.shape
