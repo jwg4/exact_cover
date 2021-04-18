6de218267cc05996b4de6d83b2ad30c733ead967
Jack Grahl <jack.grahl@gmail.com>
2021-04-19 00:34:09 +0100

    Run the same test but on generated problems.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+from hypothesis import given
+
 from exact_cover.helpers import reduce, split_problem
+
+from .test_exact_cover_problems import all_problems
 
 
 def test_reduce():
@@ -17,4 +21,17 @@ def test_split_problem():
     assert len(result) >= 2
     for sub in result:
         assert isinstance(sub, np.ndarray)
+        assert sub.shape == (1, 3)
+
+
+@given(all_problems)
+def test_split_problem(a):
+    result = list(split_problem(a, 2))
+    assert len(result) >= 2
+    x, y = a.shape
+    for sub in result:
+        assert isinstance(sub, np.ndarray)
+        x_1, y_1 = sub.shape
+        assert x_1 < x
+        assert y_1 == y
         assert sub.shape == (1, 3)
