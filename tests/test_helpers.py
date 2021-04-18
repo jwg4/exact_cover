1abf3d17de4eb8d8782fcc208201c890f337591c
Jack Grahl <jack.grahl@gmail.com>
2021-04-19 00:47:44 +0100

    Add tests for reduce.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,8 +3,9 @@ import numpy as np
 from hypothesis import given
 
 from exact_cover.helpers import reduce, split_problem
+from exact_cover.error import NoSolution, CannotSplitFurther
 
-from .test_exact_cover_problems import all_problems, array_with_solution
+from .test_exact_cover_problems import all_problems, array_with_solution, array_without_solution
 
 
 def test_reduce():
@@ -12,6 +13,23 @@ def test_reduce():
     a = np.array(d)
     result = reduce(a)
     np.testing.assert_array_equal(result, a)
+
+
+@given(array_with_solution)
+def test_reduce_with_solution(a):
+    result = reduce(a)
+    assert result.shape[0] <= a.shape[0]
+    assert result.shape[1] <= a.shape[1]
+
+
+@given(array_without_solution)
+def test_reduce_without_solution(a):
+    try:
+        result = reduce(a)
+        assert result.shape[0] <= a.shape[0]
+        assert result.shape[1] <= a.shape[1]
+    except NoSolution:
+        assert True
 
 
 def test_split_problem():
