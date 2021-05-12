09fef953fbf795c30dcd1085df533d2045b18aa7
Jack Grahl <jack.grahl@gmail.com>
2021-05-12 23:34:10 +0100

    Split into two tests.

--- tests/test_sparse_matrix.c
+++ tests/test_sparse_matrix.c
@@ -41,7 +41,7 @@ static MunitResult test_choose_column_with_min_data(const MunitParameter params[
     return MUNIT_OK;
 }
 
-static MunitResult test_matrix(const MunitParameter params[], void* data)
+static MunitResult test_cover_column(const MunitParameter params[], void* data)
 {
     int matrix[VSIZE*HSIZE] =
         {
@@ -73,7 +73,39 @@ static MunitResult test_matrix(const MunitParameter params[], void* data)
     munit_assert_int(get_data(get_right(get_right(sparse_matrix)))->data, ==, 1);   // sum for second column
     munit_assert_int(get_data(get_left(get_left(sparse_matrix)))->data, ==, 1);     // sum for 2nd-last column
 
+    destroy_entire_grid(sparse_matrix);
+    return MUNIT_OK;
+}
+
+static MunitResult test_cover_and_uncover_column(const MunitParameter params[], void* data)
+{
+    int matrix[VSIZE*HSIZE] =
+        {
+ // sums: 8, 9, 8,11, 7,10, 8, 5, 5
+          0, 1, 0, 1, 0, 0, 0, 1, 0,
+          1, 1, 1, 1, 1, 1, 1, 0, 1,
+          1, 0, 0, 1, 0, 1, 1, 1, 0,
+          1, 0, 1, 1, 0, 1, 1, 1, 0,
+          1, 1, 1, 1, 0, 0, 0, 1, 1,
+          1, 0, 1, 1, 1, 0, 1, 1, 1,
+          0, 1, 0, 1, 1, 1, 1, 0, 0,
+          0, 1, 0, 0, 1, 0, 0, 0, 0,
+          1, 1, 1, 0, 0, 0, 1, 0, 0,
+          0, 1, 1, 1, 1, 1, 1, 0, 1,
+          1, 1, 1, 0, 0, 1, 1, 0, 0,
+          0, 1, 0, 1, 1, 1, 0, 0, 1,
+          0, 0, 0, 1, 1, 1, 0, 0, 0,
+          0, 0, 0, 0, 0, 1, 0, 0, 0,
+          1, 0, 1, 1, 0, 1, 0, 0, 0
+        };
+
+    list sparse_matrix = create_sparse(VSIZE, HSIZE, matrix);
+    list column;
+
+    column = get_right(sparse_matrix);
+    cover_column(column);
     uncover_column(column);
+
     munit_assert_int(get_data(get_right(sparse_matrix))->data, ==, 8);              // sum for first column
     munit_assert_int(get_data(get_left(sparse_matrix))->data, ==, 5);               // sum for last column
     munit_assert_int(get_data(get_right(get_right(sparse_matrix)))->data, ==, 9);   // sum for second column
@@ -82,9 +114,9 @@ static MunitResult test_matrix(const MunitParameter params[], void* data)
     destroy_entire_grid(sparse_matrix);
     return MUNIT_OK;
 }
-
 static MunitTest test_suite_tests[] = {
-  { (char*) "test_covering_and_uncovering", test_matrix, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "test_covering", test_cover_column, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "test_covering_and_uncovering", test_cover_and_uncover_column, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "test_choose_column_with_min_data", test_choose_column_with_min_data, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
