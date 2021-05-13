aec72f7ee41e39447a8cf4c9c4397557f240f13e
Jack Grahl <jack.grahl@gmail.com>
2021-05-13 19:52:36 +0100

    Check the row and column sums of the sudoku problem.

--- tests/test_sudoku_based.py
+++ tests/test_sudoku_based.py
@@ -7,6 +7,10 @@ from exact_cover.helpers import is_solution
 def test_small_sudoku_problem():
     matrix = np.genfromtxt("tests/files/small_sudoku.csv", dtype=np.int32)
     assert matrix.shape == (64, 64)
+    row_sums = np.sum(matrix, axis=0)
+    assert all(row_sums == 4)
+    col_sums = np.sum(matrix, axis=1)
+    assert all(col_sums == 4)
 
 
 def test_solve_small_sudoku_problem():
