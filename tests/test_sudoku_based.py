0275622269a39af811625167a1bdf1703b662bd7
Jack Grahl <jack.grahl@gmail.com>
2021-05-13 22:51:39 +0100

    Use the canonical load function.

--- tests/test_sudoku_based.py
+++ tests/test_sudoku_based.py
@@ -2,10 +2,11 @@ import numpy as np
 
 from exact_cover import get_exact_cover
 from exact_cover.helpers import is_solution
+from exact_cover.io import load_problem
 
 
 def test_small_sudoku_problem():
-    matrix = np.genfromtxt("tests/files/small_sudoku.csv", dtype=np.int32)
+    matrix = load_problem("tests/files/small_sudoku.csv")
     assert matrix.shape == (64, 64)
     col_sums = np.sum(matrix, axis=0)
     assert all(col_sums <= 4)
@@ -14,6 +15,6 @@ def test_small_sudoku_problem():
 
 
 def test_solve_small_sudoku_problem():
-    matrix = np.genfromtxt("tests/files/small_sudoku.csv", dtype=np.int32)
+    matrix = load_problem("tests/files/small_sudoku.csv")
     solution = get_exact_cover(matrix)
     assert is_solution(solution, matrix)
