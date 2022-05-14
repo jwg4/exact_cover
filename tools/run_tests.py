bfe1079f95027d11bda84e31e9a4f341107ca17b
Jack Grahl <jack.grahl@gmail.com>
2022-05-14 16:00:34 +0100

    Add a command and an action for running quick tess only.

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -2,9 +2,17 @@ import doctest
 
 import pytest
 
+from tests.config import GLOBAL_CONFIG
+
 
 def test():
     pytest.main()
+
+
+def quicktest():
+    global GLOBAL_CONFIG
+    GLOBAL_CONFIG["SKIP_SLOW"] = True
+    pytest.main(["--fail-slow", "2s"])
 
 
 def run_doctest():
