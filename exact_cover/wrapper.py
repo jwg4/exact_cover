980a3aee6876294673bfabdc1dbfea0f9e6a33fd
John Rudge <jfr23@cam.ac.uk>
2023-01-21 16:46:25 +0000

    Linting.

--- exact_cover/wrapper.py
+++ exact_cover/wrapper.py
@@ -3,6 +3,7 @@ from exact_cover_impl import get_exact_cover as raw_get_exact_cover
 from .error import NoSolution
 from .io import DTYPE_FOR_ARRAY
 
+
 def get_exact_cover(matrix):
     if matrix.dtype == DTYPE_FOR_ARRAY:
         result = raw_get_exact_cover(matrix)
