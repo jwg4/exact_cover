8d10f29e87666f9e206934917eafac61c51cd121
John Rudge <jfr23@cam.ac.uk>
2023-01-21 16:27:28 +0000

    Do casting for appropriate dtype in wrapper.

--- exact_cover/wrapper.py
+++ exact_cover/wrapper.py
@@ -1,10 +1,13 @@
 from exact_cover_impl import get_exact_cover as raw_get_exact_cover
 
 from .error import NoSolution
-
+from .io import DTYPE_FOR_ARRAY
 
 def get_exact_cover(matrix):
-    result = raw_get_exact_cover(matrix)
+    if matrix.dtype == DTYPE_FOR_ARRAY:
+        result = raw_get_exact_cover(matrix)
+    else:
+        result = raw_get_exact_cover(matrix.astype(DTYPE_FOR_ARRAY))
     if result.size == 0:
         raise NoSolution("No solutions found by the C code.")
     return result
