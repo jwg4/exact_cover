e288502e0e68f57581fa160729f304d32e635ba8
Jack Grahl <jack.grahl@gmail.com>
2023-02-03 23:53:34 +0000

    Correct expected array.

--- exact_cover/helpers.py
+++ exact_cover/helpers.py
@@ -42,7 +42,8 @@ def reduce(a):
 def create_numpy_array(a):
     """
     >>> create_numpy_array([[0, 1], [1, 0]])
-    numpy.array([[0, 1], [1, 0]])
+    array([[False,  True],
+           [ True, False]])
     """
     return np.array(a, dtype=DTYPE_FOR_ARRAY)
 
