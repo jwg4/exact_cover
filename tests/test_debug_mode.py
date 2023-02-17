7bcf5444b57f569ec0b0627c00ede3d1695c516b
Jack Grahl <jack.grahl@gmail.com>
2023-02-17 11:40:21 +0000

    Reformatting.

--- tests/test_debug_mode.py
+++ tests/test_debug_mode.py
@@ -4,30 +4,21 @@ from exact_cover import get_exact_cover
 
 
 def test_exact_cover_default():
-    data = np.array(
-        [[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]],
-        dtype=np.int32
-    )
+    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]], dtype=np.int32)
     expected = np.array([0, 1, 3])
     actual = get_exact_cover(data)
     np.testing.assert_array_equal(actual, expected)
 
 
 def test_exact_cover_false():
-    data = np.array(
-        [[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]],
-        dtype=np.int32
-    )
+    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]], dtype=np.int32)
     expected = np.array([0, 1, 3])
     actual = get_exact_cover(data, False)
     np.testing.assert_array_equal(actual, expected)
 
 
 def test_exact_cover_true():
-    data = np.array(
-        [[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]],
-        dtype=np.int32
-    )
+    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]], dtype=np.int32)
     expected = np.array([0, 1, 3])
     actual, debug_info = get_exact_cover(data, True)
     np.testing.assert_array_equal(actual, expected)
