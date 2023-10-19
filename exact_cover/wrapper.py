36bef575bd7b6e077c5cbbdcac5d9163ba14f7c6
Jack Grahl <jack.grahl@gmail.com>
2023-10-19 23:18:03 +0100

    Add an assertion to help us pinpoint the error case.

--- exact_cover/wrapper.py
+++ exact_cover/wrapper.py
@@ -6,9 +6,12 @@ from .io import DTYPE_FOR_ARRAY
 
 def get_exact_cover(matrix):
     if matrix.dtype == DTYPE_FOR_ARRAY:
+        assert matrix.flags.c_contiguous, "We depend on the input array being C contiguous as we access the internal data directly."
         result = raw_get_exact_cover(matrix)
     else:
-        result = raw_get_exact_cover(matrix.astype(DTYPE_FOR_ARRAY))
+        transformed = matrix.astype(DTYPE_FOR_ARRAY)
+        assert transformed.flags.c_contiguous, "We depend on the input array being C contiguous as we access the internal data directly."
+        result = raw_get_exact_cover(transformed)
     if result.size == 0:
         raise NoSolution("No solutions found by the C code.")
     return result
