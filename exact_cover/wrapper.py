2eac20d2edea219352e79fc6b8f082426078f241
Jack Grahl <jack.grahl@gmail.com>
2023-10-20 22:30:07 +0100

    Apply the correct transforms in every case
    ... and let numpy decide whether to copy array or not.

--- exact_cover/wrapper.py
+++ exact_cover/wrapper.py
@@ -3,15 +3,13 @@ from exact_cover_impl import get_exact_cover as raw_get_exact_cover
 from .error import NoSolution
 from .io import DTYPE_FOR_ARRAY
 
+import numpy as np
+
 
 def get_exact_cover(matrix):
-    if matrix.dtype == DTYPE_FOR_ARRAY:
-        assert matrix.flags.c_contiguous, "We depend on the input array being C contiguous as we access the internal data directly."
-        result = raw_get_exact_cover(matrix)
-    else:
-        transformed = matrix.astype(DTYPE_FOR_ARRAY)
-        assert transformed.flags.c_contiguous, "We depend on the input array being C contiguous as we access the internal data directly."
-        result = raw_get_exact_cover(transformed)
+    transformed = np.require(matrix, dtype=DTYPE_FOR_ARRAY, requirements=['C_CONTIGUOUS'])
+    assert transformed.flags.c_contiguous, "We depend on the input array being C contiguous as we access the internal data directly."
+    result = raw_get_exact_cover(transformed)
     if result.size == 0:
         raise NoSolution("No solutions found by the C code.")
     return result
