d214e73b1ff6b28def38dae88b9cc88ae7e2b8a1
Jack Grahl <jack.grahl@gmail.com>
2023-11-15 00:11:49 +0000

    Add two more similar tests.

--- tests/test_solution_count.py
+++ tests/test_solution_count.py
@@ -8,3 +8,15 @@ def test_solution_count():
     data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]], dtype=DTYPE_FOR_ARRAY)
     result = get_solution_count(data)
     assert result == 2
+
+
+def test_solution_count_single_solution():
+    data = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 1]], dtype=DTYPE_FOR_ARRAY)
+    result = get_solution_count(data)
+    assert result == 1
+
+
+def test_solution_count_no_solutions():
+    data = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0]], dtype=DTYPE_FOR_ARRAY)
+    result = get_solution_count(data)
+    assert result == 0
