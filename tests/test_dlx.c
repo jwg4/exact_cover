1d351f212814a4a719fc52819d6227a416114b81
Jack Grahl <jack.grahl@gmail.com>
2023-11-18 23:37:13 +0000

    Add a test for solution count.

--- tests/test_dlx.c
+++ tests/test_dlx.c
@@ -58,6 +58,22 @@ test_get_multiple_row_result(const MunitParameter params[], void* data) {
 }
 
 static MunitResult
+test_get_solution_count(const MunitParameter params[], void* data) {
+  (void) params;
+  (void) data;
+
+  int row_count = 4;
+  int col_count = 3;
+  char array[] = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1};
+  int solution[] = {0, 0, 0, 0};
+
+  int count = dlx_get_solution_count(row_count, col_count, array);
+  munit_assert(count == 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
 test_correct_zeroing_out(const MunitParameter params[], void* data) {
   (void) params;
   (void) data;
@@ -101,6 +117,7 @@ static MunitTest test_suite_tests[] = {
   { (char*) "one_row_problem", test_get_one_row_result, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "two_row_problem", test_get_two_row_result, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "bigger_simple_problem", test_get_multiple_row_result, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { (char*) "count_single_solution", test_get_solution_count, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "zero_out_array_correctly", test_correct_zeroing_out, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { (char*) "zero_out_array_start_and_end", test_correct_zeroing_out_2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
