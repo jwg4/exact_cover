1dcf285770fd045e7726a4692100876b8e1a20b6
Jack Grahl <jack.grahl@gmail.com>
2023-11-18 17:54:57 +0000

    Count solutions in C code.

--- src/dlx.c
+++ src/dlx.c
@@ -42,6 +42,35 @@ int search(list sparse_matrix, int k, int max, int *solution) {
     return result;
 }
 
+int count(list sparse_matrix, int k, int max, int *solution) {
+    list col, row, next;
+    int result = 0;
+    int c = 0;
+
+    // Base cases:
+    // 1. There are no columns left; we've found a solution.
+    if (get_right(sparse_matrix) == sparse_matrix) return 1;
+    // 2. There's a column with only zeros. This branch of the search
+    //    tree has no solutions and we need to backtrack.
+    col = choose_column_with_min_data(sparse_matrix, max);
+    if (get_data(col)->data == 0) return 0;
+
+    // Main algorithm:
+    cover_column(col);
+    for (row = col; (row = get_down(row)) != col; ) {
+        solution[k] = get_data(row)->data;  // save the row number
+        for (next = row; (next = get_right(next)) != row; )
+            cover_column(get_data(next)->list_data);
+        result = count(sparse_matrix, k+1, max, solution);
+        c += result;
+        // If result > 0 we're done, but we should still clean up.
+        for (next = row; (next = get_left(next)) != row; )
+            uncover_column(get_data(next)->list_data);
+    }
+    uncover_column(col);
+    return c;
+}
+
 int dlx_get_exact_cover(int rows, int cols, char matrix[], int *solution) {
     list sparse_matrix;
     int solution_length;
@@ -65,16 +94,8 @@ int dlx_get_solution_count(int rows, int cols, char matrix[]) {
     int *solution = malloc(rows * sizeof(*solution));
 
     sparse_matrix = create_sparse(rows, cols, matrix);
-    while (solution_length > 0){
-        solution_count++;
-        solution_length = search(sparse_matrix, 0, rows, solution);
-    }
+    solution_count = count(sparse_matrix, 0, rows, solution);
     destroy_entire_grid(sparse_matrix);
-
-    while (rows > solution_length) {
-        solution[--rows] = 0;  // Zero out everything above position i.  Caller
-                               // can use this to determine if there is a solution.
-    };
 
     free(solution);
     return solution_count;
