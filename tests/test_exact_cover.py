9db92d204bbaf04df5f882c3806d9549ece7feaa
Jack Grahl <jack.grahl@gmail.com>
2023-11-18 23:58:22 +0000

    Break line.

--- tests/test_exact_cover.py
+++ tests/test_exact_cover.py
@@ -19,7 +19,10 @@ def test_exact_cover():
 
 
 def test_exact_cover_with_0_rows():
-    data = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 0], [0, 0, 1]], dtype=DTYPE_FOR_ARRAY)
+    data = np.array(
+        [[1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 0], [0, 0, 1]],
+        dtype=DTYPE_FOR_ARRAY,
+    )
     expected = np.array([0, 2, 5])
     actual = get_exact_cover(data)
     np.testing.assert_array_equal(actual, expected)
