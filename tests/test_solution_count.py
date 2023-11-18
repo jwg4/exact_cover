74283222b8823935fc5d5a726525066d6583af64
Jack Grahl <jack.grahl@gmail.com>
2023-11-18 23:40:40 +0000

    Fix long lines.

--- tests/test_solution_count.py
+++ tests/test_solution_count.py
@@ -16,27 +16,33 @@ def test_solution_count():
 
 def test_solution_count_zero_rows():
     """
-        Blank rows are ignored - we don't count extra solutions
-        which include some collection of blank rows.
+    Blank rows are ignored - we don't count extra solutions
+    which include some collection of blank rows.
     """
-    data = np.array([[1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 0], [0, 0, 1]], dtype=DTYPE_FOR_ARRAY)
+    data = np.array(
+        [[1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 0], [0, 0, 1]],
+        dtype=DTYPE_FOR_ARRAY,
+    )
     result = get_solution_count(data)
     assert result == 2
 
 
 def test_solution_count_duplicate_rows():
     """
-        Duplicate rows mean that all the solutions including
-        those rows are counted multiple times.
+    Duplicate rows mean that all the solutions including
+    those rows are counted multiple times.
 
-        Here we have a basic problem involving rows
-        A, B, C, and D, and two distinct solutions, A+C and A+B+D
+    Here we have a basic problem involving rows
+    A, B, C, and D, and two distinct solutions, A+C and A+B+D
 
-        By adding another copy of A and another copy of C, we
-        get 6 different solutions:
-        A1 + C1, A1 + C2, A2 + C1, A2 + C2, A1 + B + D, A2 + B + D
+    By adding another copy of A and another copy of C, we
+    get 6 different solutions:
+    A1 + C1, A1 + C2, A2 + C1, A2 + C2, A1 + B + D, A2 + B + D
     """
-    data = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 1, 1], [0, 1, 1], [0, 0, 1]], dtype=DTYPE_FOR_ARRAY)
+    data = np.array(
+        [[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 1, 1], [0, 1, 1], [0, 0, 1]],
+        dtype=DTYPE_FOR_ARRAY,
+    )
     result = get_solution_count(data)
     assert result == 6
 
