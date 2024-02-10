6e4b4e538ef2c1300c62fcc8165f5998b7dfcb27
Jack Grahl <jack.grahl@gmail.com>
2024-02-10 16:56:57 +0000

    Correct how we call the C code.

--- exact_cover/wrapper.py
+++ exact_cover/wrapper.py
@@ -1,4 +1,5 @@
 from exact_cover_impl import get_exact_cover as raw_get_exact_cover
+from exact_cover_impl import get_solution_count as raw_get_exact_cover
 from exact_cover_impl import get_all_solutions as raw_get_all_solutions
 
 from .error import NoSolution
@@ -27,7 +28,8 @@ def get_all_solutions(matrix):
     assert (
         transformed.flags.c_contiguous
     ), "We depend on the input array being C contiguous for raw goodness."
-    result = raw_get_all_solutions(transformed)
+    count = raw_get_solution_count(transformed)
+    result = raw_get_all_solutions(transformed, count)
     if result.size == 0:
         raise NoSolution("No solutions found by the C code.")
     return result
