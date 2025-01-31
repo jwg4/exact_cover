e2daa564c67b232a52bdff448935a1669cab812b
Jack Grahl <jack.grahl@gmail.com>
2025-01-31 08:00:03 +0000

    Spaces in single element tuples.

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -10,7 +10,7 @@ def test_solutions_array_to_set():
     input_array[1, 1] = 9
 
     actual = solutions_array_to_set(input_array)
-    expected = {(7, ), (8, ), (8, 9)}
+    expected = {(7,), (8,), (8, 9)}
     assert actual == expected
 
 
@@ -20,5 +20,5 @@ def test_solutions_array_to_set_wide():
     input_array[1, 0] = input_array[2, 0] = 8
 
     actual = solutions_array_to_set(input_array)
-    expected = {(7, ), (8, )}
+    expected = {(7,), (8,)}
     assert actual == expected
