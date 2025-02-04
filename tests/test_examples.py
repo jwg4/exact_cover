660b0a3a18c1fbf8ca18c66125e842466d769198
Jack Grahl <jack.grahl@gmail.com>
2025-02-04 22:40:20 +0000

    Correct how we do this.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,4 +1,5 @@
 from exact_cover import get_all_solutions
+from exact_cover.error import NoSolution
 
 from exact_cover_samples import problems
 from pytest import mark
@@ -10,5 +11,8 @@ SAMPLE_PROBLEMS = [problems[name]() for name in problems]
     SAMPLE_PROBLEMS,)  
 def test_exact_cover_solve(problem):
     data = problem["data"]
-    result = get_all_solutions(data)
-    assert result == problem["data"]  
+    try:
+        result = get_all_solutions(data)
+    except NoSolution:
+        result = []
+    assert result == problem["solutions"]  
