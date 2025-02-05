e95370f5ae067580dc0b50e7722c7a45df7efa95
Jack Grahl <jack.grahl@gmail.com>
2025-02-05 08:42:29 +0000

    Run ECS tests optionally.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,17 +1,22 @@
 from exact_cover import get_all_solutions
 from exact_cover.error import NoSolution
 
-from exact_cover_samples import problems, canonical
 from pytest import mark
+
+try:
+    from exact_cover_samples import problems, canonical
+    RUN_SAMPLES = True
+except ImportError:
+    RUN_SAMPLES = False
 
 
 SAMPLE_PROBLEMS = [problems[name]() for name in problems]
 
-@mark.parametrize("problem",  
-    SAMPLE_PROBLEMS,)  
+@mark.parametrize("problem", SAMPLE_PROBLEMS,)
+@mark.skipif(not RUN_SAMPLES, reason="Not running exact-cover-samples")
 def test_exact_cover_solve(problem):
-
     data = problem["data"]
+
     try:
         result = get_all_solutions(data)
     except NoSolution:
