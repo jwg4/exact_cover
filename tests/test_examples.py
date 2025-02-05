9f3558e9a3500e8386528449a3a1801a9154c0fc
Jack Grahl <jack.grahl@gmail.com>
2025-02-05 09:10:41 +0000

    Formatting.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,6 +5,7 @@ from pytest import mark
 
 try:
     from exact_cover_samples import problems, canonical
+
     RUN_SAMPLES = True
 except ImportError:
     problems = []
@@ -13,7 +14,8 @@ except ImportError:
 
 SAMPLE_PROBLEMS = [problems[name]() for name in problems]
 
-@mark.parametrize("problem", SAMPLE_PROBLEMS,)
+
+@mark.parametrize("problem", SAMPLE_PROBLEMS)
 @mark.skipif(not RUN_SAMPLES, reason="Not running exact-cover-samples")
 def test_exact_cover_solve(problem):
     data = problem["data"]
@@ -22,7 +24,7 @@ def test_exact_cover_solve(problem):
         result = get_all_solutions(data)
     except NoSolution:
         result = []
-    
+
     expected = problem["solutions"]
 
     assert canonical(result) == canonical(expected)
